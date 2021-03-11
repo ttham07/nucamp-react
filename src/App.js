c49bd9388abef2b1274a2561978e0aaa0c850b11
Tommy Tham <tommytham@theShopMBPro.attlocal.net>
2021-03-10 19:14:40 -0500

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryComponent';
+import Main from './components/MainComponents';
 import './App.css';
 
-class App extends Component {
+class App extends Component {   
     render() {
         return (
             <div className="App">
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
-                </Navbar>
-                <Directory />
+                <Main />
             </div>
         );
     }
